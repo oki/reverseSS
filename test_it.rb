dd58aa76bf3dee4b3c7dd280bbf564e8d64220ff
Michał Kurek <oki@md6.org>
2012-02-27 10:25:01 +0100

    Init commit

---
+++ test_it.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'bindata'
+require 'pp'
+
+io = IO.read('packet.dump')
+
+class Workout < BinData::Record
+    #endian :little
+    endian :big
+
+    uint32 :versionNumber
+    uint32 :headerSize
+    uint32 :applicationId
+    uint32 :machineId
+    uint32 :features 
+
+    uint16 :featureFlags
+    uint16 :sharingFlags
+    uint32 :workoutId
+end
+
+w = Workout.read(io)
+
+puts "versionNumber: #{w.versionNumber}"
+puts "headerSize: #{w.headerSize}"
+puts "applicationId: #{w.applicationId}"
+puts "machineId: #{w.machineId}"
+puts "features: #{w.features}"
+puts "featureFlags: #{w.featureFlags}"
+puts "sharingFlags: #{w.sharingFlags}"
+puts "workoutId: #{w.workoutId}"
