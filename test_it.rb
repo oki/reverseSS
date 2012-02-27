68306d991ca668e50bd4259ebad4e227a6d3654d
Michał Kurek <oki@md6.org>
2012-02-27 19:19:16 +0100

    Added packet dump with gpx file included. Added AsString as BinData::Primitive

--- test_it.rb
+++ test_it.rb
@@ -5,10 +5,24 @@ require "bundler/setup"
 require 'bindata'
 require 'pp'
 
-io = IO.read('packet.dump')
+# io = IO.read('packet.dump')
+io = IO.read('with-gpx.dump')
+
+class AsString < BinData::Primitive
+    endian :big
+    uint16 :len, :value => lambda { data.length }
+    string :data, :read_length => :len
+
+    def get
+        self.data
+    end
+
+    def set(v)
+        self.data = v
+    end
+end
 
 class Workout < BinData::Record
-    #endian :little
     endian :big
 
     uint32 :versionNumber
@@ -20,15 +34,39 @@ class Workout < BinData::Record
     uint16 :featureFlags
     uint16 :sharingFlags
     uint32 :workoutId
+
+    as_string :description
+    as_string :workoutkey
+
+    uint32 :userid 
+    uint32 :routeId 
+    uint32 :activityId
+
+    as_string :username
+    as_string :workoutName
+
+    uint32 :timeOffset
+    double :startTime
+    double :stopTime
+
+    uint32 :totalTime
+    uint32 :totalDistance
+    uint32 :weight
+
+    uint32 :energyConsumed
+
+    bit32 :measurementUnit
+
+    as_string :mcc
+    as_string :mnc
 end
 
 w = Workout.read(io)
 
-puts "versionNumber: #{w.versionNumber}"
-puts "headerSize: #{w.headerSize}"
-puts "applicationId: #{w.applicationId}"
-puts "machineId: #{w.machineId}"
-puts "features: #{w.features}"
-puts "featureFlags: #{w.featureFlags}"
-puts "sharingFlags: #{w.sharingFlags}"
-puts "workoutId: #{w.workoutId}"
+if w.featureFlags & 1 << 11
+    puts "isManuallyAdded"
+end
+
+w.field_names.each do |field_name|
+    puts "#{field_name}: #{w.send(field_name)}"
+end
